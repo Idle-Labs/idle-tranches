129509d8381e0c3cf4dc918b93682e4d996fdb65
Akshay Meher <akshay@Akshays-MacBook-Pro.local>
2022-01-03 20:07:10 +0530

    Restruct the repo

--- test/integration/mstable/IdleMStableStrategyWrapper.js
+++ test/integration/mstable/IdleMStableStrategyWrapper.js
@@ -1,17 +1,19 @@
 require("hardhat/config");
 const { BigNumber } = require("@ethersproject/bignumber");
-const helpers = require("../scripts/helpers");
-const addresses = require("../lib/addresses");
-const stkAAVEjson = require("../artifacts/contracts/interfaces/IStakedAave.sol/IStakedAave.json");
+const helpers = require("../../../scripts/helpers");
+const addresses = require("../../../lib/addresses");
+const stkAAVEjson = require("../../../artifacts/contracts/interfaces/IStakedAave.sol/IStakedAave.json");
 const { expect } = require("chai");
 const { FakeContract, smock } = require("@defi-wonderland/smock");
-const erc20 = require("../artifacts/contracts/interfaces/IERC20Detailed.sol/IERC20Detailed.json");
+const erc20 = require("../../../artifacts/contracts/interfaces/IERC20Detailed.sol/IERC20Detailed.json");
 
 require("chai").use(smock.matchers);
 
 const BN = (n) => BigNumber.from(n.toString());
 const ONE_TOKEN = (n, decimals) => BigNumber.from("10").pow(BigNumber.from(n));
-const MAX_UINT = BN("115792089237316195423570985008687907853269984665640564039457584007913129639935");
+const MAX_UINT = BN(
+  "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+);
 
 const AMOUNT_TO_TRANSFER = BN("10000").mul(ONE_TOKEN(18));
 const AMOUNT_TO_TEST = BN("1").mul(ONE_TOKEN(18));
@@ -48,8 +50,14 @@ describe.only("IdleMStable Strategy Wrapper", function () {
 
     dai_signer = await ethers.getSigner(dai_whale);
     DAI = await ethers.getContractAt(erc20.abi, DAIAddress);
-    await DAI.connect(dai_signer).transfer(AABuyer.address, AMOUNT_TO_TRANSFER.mul(10));
-    await DAI.connect(dai_signer).transfer(BBBuyer.address, AMOUNT_TO_TRANSFER.mul(10));
+    await DAI.connect(dai_signer).transfer(
+      AABuyer.address,
+      AMOUNT_TO_TRANSFER.mul(10)
+    );
+    await DAI.connect(dai_signer).transfer(
+      BBBuyer.address,
+      AMOUNT_TO_TRANSFER.mul(10)
+    );
 
     one = ONE_TOKEN(18);
     const IdleCDOTranche = await ethers.getContractFactory("IdleCDOTranche");
@@ -75,8 +83,16 @@ describe.only("IdleMStable Strategy Wrapper", function () {
     idleToken2 = await MockIdleToken.deploy(underlying.address);
     await idleToken2.deployed();
 
-    strategy = await helpers.deployUpgradableContract("IdleStrategy", [idleToken.address, owner.address], owner);
-    strategy2 = await helpers.deployUpgradableContract("IdleStrategy", [idleToken2.address, owner.address], owner);
+    strategy = await helpers.deployUpgradableContract(
+      "IdleStrategy",
+      [idleToken.address, owner.address],
+      owner
+    );
+    strategy2 = await helpers.deployUpgradableContract(
+      "IdleStrategy",
+      [idleToken2.address, owner.address],
+      owner
+    );
     idleCDO = await helpers.deployUpgradableContract(
       "EnhancedIdleCDO",
       [
@@ -96,8 +112,14 @@ describe.only("IdleMStable Strategy Wrapper", function () {
     await idleCDO.setWethForTest(weth.address);
     await idleCDO.setUniRouterForTest(uniRouter.address);
 
-    AA = await ethers.getContractAt("IdleCDOTranche", await idleCDO.AATranche());
-    BB = await ethers.getContractAt("IdleCDOTranche", await idleCDO.BBTranche());
+    AA = await ethers.getContractAt(
+      "IdleCDOTranche",
+      await idleCDO.AATranche()
+    );
+    BB = await ethers.getContractAt(
+      "IdleCDOTranche",
+      await idleCDO.BBTranche()
+    );
 
     const stakingRewardsParams = [
       incentiveTokens,
@@ -106,9 +128,20 @@ describe.only("IdleMStable Strategy Wrapper", function () {
       owner.address, // recovery address
       10, // cooling period
     ];
-    stakingRewardsAA = await helpers.deployUpgradableContract("IdleCDOTrancheRewards", [AA.address, ...stakingRewardsParams], owner);
-    stakingRewardsBB = await helpers.deployUpgradableContract("IdleCDOTrancheRewards", [BB.address, ...stakingRewardsParams], owner);
-    await idleCDO.setStakingRewards(stakingRewardsAA.address, stakingRewardsBB.address);
+    stakingRewardsAA = await helpers.deployUpgradableContract(
+      "IdleCDOTrancheRewards",
+      [AA.address, ...stakingRewardsParams],
+      owner
+    );
+    stakingRewardsBB = await helpers.deployUpgradableContract(
+      "IdleCDOTrancheRewards",
+      [BB.address, ...stakingRewardsParams],
+      owner
+    );
+    await idleCDO.setStakingRewards(
+      stakingRewardsAA.address,
+      stakingRewardsBB.address
+    );
 
     await idleCDO.setUnlentPerc(BN("0"));
     await idleCDO.setIsStkAAVEActive(false);
@@ -118,60 +151,105 @@ describe.only("IdleMStable Strategy Wrapper", function () {
     // set IdleToken2 mocked params
     await idleToken2.setTokenPriceWithFee(BN(2 * 10 ** 18));
 
-    let idleMStableStrategyWrapperFactory = await ethers.getContractFactory("IdleMStableStrategyWrapper");
-    idleMStableStrategyWrapper = await idleMStableStrategyWrapperFactory.deploy(mUSD_ADDRESS, idleCDO.address);
+    let idleMStableStrategyWrapperFactory = await ethers.getContractFactory(
+      "IdleMStableStrategyWrapper"
+    );
+    idleMStableStrategyWrapper = await idleMStableStrategyWrapperFactory.deploy(
+      mUSD_ADDRESS,
+      idleCDO.address
+    );
   });
 
   it("Deposit with with AA token", async () => {
     let AATrancheTokenAddress = await idleCDO.AATranche();
-    let aaTokenContract = await ethers.getContractAt(erc20.abi, AATrancheTokenAddress);
+    let aaTokenContract = await ethers.getContractAt(
+      erc20.abi,
+      AATrancheTokenAddress
+    );
     let aaTokenBalanceBefore = await aaTokenContract.balanceOf(AABuyer.address);
-    await DAI.connect(AABuyer).approve(idleMStableStrategyWrapper.address, AMOUNT_TO_TEST);
-    await idleMStableStrategyWrapper.connect(AABuyer).depositAAWithToken(DAI.address, AMOUNT_TO_TEST, "1");
+    await DAI.connect(AABuyer).approve(
+      idleMStableStrategyWrapper.address,
+      AMOUNT_TO_TEST
+    );
+    await idleMStableStrategyWrapper
+      .connect(AABuyer)
+      .depositAAWithToken(DAI.address, AMOUNT_TO_TEST, "1");
     let aaTokenBalanceAfter = await aaTokenContract.balanceOf(AABuyer.address);
     expect(aaTokenBalanceAfter.sub(aaTokenBalanceBefore)).gt(0);
   });
 
   it("Deposit with BB Token", async () => {
     let BBTrancheTokenAddress = await idleCDO.BBTranche();
-    let bbTokenContract = await ethers.getContractAt(erc20.abi, BBTrancheTokenAddress);
+    let bbTokenContract = await ethers.getContractAt(
+      erc20.abi,
+      BBTrancheTokenAddress
+    );
     let bbTokenBalanceBefore = await bbTokenContract.balanceOf(BBBuyer.address);
-    await DAI.connect(BBBuyer).approve(idleMStableStrategyWrapper.address, AMOUNT_TO_TEST);
-    await idleMStableStrategyWrapper.connect(BBBuyer).depositBBWithToken(DAI.address, AMOUNT_TO_TEST, "1");
+    await DAI.connect(BBBuyer).approve(
+      idleMStableStrategyWrapper.address,
+      AMOUNT_TO_TEST
+    );
+    await idleMStableStrategyWrapper
+      .connect(BBBuyer)
+      .depositBBWithToken(DAI.address, AMOUNT_TO_TEST, "1");
     let bbTokenBalanceAfter = await bbTokenContract.balanceOf(BBBuyer.address);
     expect(bbTokenBalanceAfter.sub(bbTokenBalanceBefore)).gt(0);
   });
 
   it("Withdraw AA Token", async () => {
     let AATrancheTokenAddress = await idleCDO.AATranche();
-    let aaTokenContract = await ethers.getContractAt(erc20.abi, AATrancheTokenAddress);
+    let aaTokenContract = await ethers.getContractAt(
+      erc20.abi,
+      AATrancheTokenAddress
+    );
     let aaTokenBalanceBefore = await aaTokenContract.balanceOf(AABuyer.address);
-    await DAI.connect(AABuyer).approve(idleMStableStrategyWrapper.address, AMOUNT_TO_TEST);
-    await idleMStableStrategyWrapper.connect(AABuyer).depositAAWithToken(DAI.address, AMOUNT_TO_TEST, "1");
+    await DAI.connect(AABuyer).approve(
+      idleMStableStrategyWrapper.address,
+      AMOUNT_TO_TEST
+    );
+    await idleMStableStrategyWrapper
+      .connect(AABuyer)
+      .depositAAWithToken(DAI.address, AMOUNT_TO_TEST, "1");
     let aaTokenBalanceAfter = await aaTokenContract.balanceOf(AABuyer.address);
 
     let aaTokensReceived = aaTokenBalanceAfter.sub(aaTokenBalanceBefore);
 
     let daiBalanceBefore = await DAI.balanceOf(AABuyer.address);
-    await aaTokenContract.connect(AABuyer).approve(idleMStableStrategyWrapper.address, aaTokensReceived);
-    await idleMStableStrategyWrapper.connect(AABuyer).withdrawTokenViaBurningAA(DAI.address, aaTokensReceived, "1");
+    await aaTokenContract
+      .connect(AABuyer)
+      .approve(idleMStableStrategyWrapper.address, aaTokensReceived);
+    await idleMStableStrategyWrapper
+      .connect(AABuyer)
+      .withdrawTokenViaBurningAA(DAI.address, aaTokensReceived, "1");
     let daiBalanceAfter = await DAI.balanceOf(AABuyer.address);
     expect(daiBalanceAfter.sub(daiBalanceBefore)).gt(0);
   });
 
   it("Withdraw BB Token", async () => {
     let BBTrancheTokenAddress = await idleCDO.BBTranche();
-    let bbTokenContract = await ethers.getContractAt(erc20.abi, BBTrancheTokenAddress);
+    let bbTokenContract = await ethers.getContractAt(
+      erc20.abi,
+      BBTrancheTokenAddress
+    );
     let bbTokenBalanceBefore = await bbTokenContract.balanceOf(BBBuyer.address);
-    await DAI.connect(BBBuyer).approve(idleMStableStrategyWrapper.address, AMOUNT_TO_TEST);
-    await idleMStableStrategyWrapper.connect(BBBuyer).depositBBWithToken(DAI.address, AMOUNT_TO_TEST, "1");
+    await DAI.connect(BBBuyer).approve(
+      idleMStableStrategyWrapper.address,
+      AMOUNT_TO_TEST
+    );
+    await idleMStableStrategyWrapper
+      .connect(BBBuyer)
+      .depositBBWithToken(DAI.address, AMOUNT_TO_TEST, "1");
     let bbTokenBalanceAfter = await bbTokenContract.balanceOf(BBBuyer.address);
 
     let bbTokensReceived = bbTokenBalanceAfter.sub(bbTokenBalanceBefore);
 
     let daiBalanceBefore = await DAI.balanceOf(BBBuyer.address);
-    await bbTokenContract.connect(BBBuyer).approve(idleMStableStrategyWrapper.address, bbTokensReceived);
-    await idleMStableStrategyWrapper.connect(BBBuyer).withdrawTokenViaBurningBB(DAI.address, bbTokensReceived, "1");
+    await bbTokenContract
+      .connect(BBBuyer)
+      .approve(idleMStableStrategyWrapper.address, bbTokensReceived);
+    await idleMStableStrategyWrapper
+      .connect(BBBuyer)
+      .withdrawTokenViaBurningBB(DAI.address, bbTokensReceived, "1");
     let daiBalanceAfter = await DAI.balanceOf(BBBuyer.address);
     expect(daiBalanceAfter.sub(daiBalanceBefore)).gt(0);
   });
