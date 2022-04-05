eee1f083b183e1eb27747970227d8dd0f6aadc54
bugduino <william@idle.finance>
2022-04-05 11:31:41 +0200

    FIX: add IIdleCDO interface

--- contracts/interfaces/IIdleCDO.sol
+++ contracts/interfaces/IIdleCDO.sol
@@ -2,5 +2,27 @@
 pragma solidity 0.8.10;
 
 interface IIdleCDO {
-  function redeemRewards() external;
+  function AATranche() external view returns(address);
+  function BBTranche() external view returns(address);
+  function AAStaking() external view returns(address);
+  function BBStaking() external view returns(address);
+  function strategy() external view returns(address);
+  function strategyToken() external view returns(address);
+  function token() external view returns(address);
+
+  function allowAAWithdraw() external view returns(bool);
+  function allowBBWithdraw() external view returns(bool);
+  function fee() external view returns(uint256);
+  function getApr(address _tranche) external view returns(uint256);
+  function getContractValue() external view returns(uint256);
+  function trancheAPRSplitRatio() external view returns(uint256);
+  function getCurrentAARatio() external view returns(uint256);
+  function tranchePrice(address _tranche) external view returns(uint256);
+  function virtualPrice(address _tranche) external view returns(uint256);
+  function getIncentiveTokens() external view returns(address[] memory);
+
+  function depositAA(uint256) external returns(uint256);
+  function depositBB(uint256) external returns(uint256);
+  function withdrawAA(uint256) external returns(uint256);
+  function withdrawBB(uint256) external returns(uint256);
 }
