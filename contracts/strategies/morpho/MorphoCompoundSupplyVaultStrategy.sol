d5b511171bc25b69af8189f69dbf5d3f718cd6f8
massun-onibakuchi <etherdao0x00@gmail.com>
2022-10-25 13:58:57 +0900

    tests: add MorphoCompond tests

--- contracts/strategies/morpho/MorphoCompoundSupplyVaultStrategy.sol
+++ contracts/strategies/morpho/MorphoCompoundSupplyVaultStrategy.sol
@@ -5,9 +5,11 @@ import "./MorphoSupplyVaultStrategy.sol";
 import "../../interfaces/morpho/IMorphoCompoundLens.sol";
 
 contract MorphoCompoundSupplyVaultStrategy is MorphoSupplyVaultStrategy {
-    uint256 public constant NBLOCKS_PER_YEAR = 2614925;
+    /// @notice average number of blocks per year
+    /// average block time is 12.06 secs after the merge
+    uint256 public constant NBLOCKS_PER_YEAR = 2614925; // 24*365*3600 / 12.06
     /// @notice address of the MorphoSupplyVault
-    IMorphoCompoundLens public COMPOUND_LENS = IMorphoCompoundLens(0x507fA343d0A90786d86C7cd885f5C49263A91FF4);
+    IMorphoCompoundLens public COMPOUND_LENS = IMorphoCompoundLens(0x930f1b46e1D081Ec1524efD95752bE3eCe51EF67);
 
     function getApr() external view override returns (uint256 apr) {
         //  The market's average supply rate per block (in wad).
