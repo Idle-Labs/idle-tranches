699640fcd4119b550298f8d23284879714ec80b8
bugduino <william@idle.finance>
2023-06-12 12:02:25 +0200

    WIP: add support for polygon zkEVM with clearpool PYT

---
+++ contracts/polygon-zk/IdleCDOPolygonZK.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+import "../interfaces/IERC20Detailed.sol";
+import "../IdleCDO.sol";
+
+/// @title A perpetual tranche implementation, deployed on Polygon zkEVM
+/// @author Idle Labs Inc.
+/// @notice More info and high level overview in the README
+/// @dev The contract is upgradable, to add storage slots, create IdleCDOStorageVX and inherit from IdleCDOStorage, then update the definitaion below
+contract IdleCDOPolygonZK is IdleCDO {
+  using SafeERC20Upgradeable for IERC20Detailed;
+
+  /// @notice used by child contracts (cdo variants) if anything needs to be done on/after init
+  function _additionalInit() internal override {
+    weth = address(0x4F9A0e7FD2Bf6067db6994CF12E4495Df938E6e9);
+    feeReceiver = address(0x13854835c508FC79C3E5C5Abf7afa54b4CcC1Fdf); // treasury multisig
+  }
+
+  /// @notice method used to sell `_rewardToken` for `_token` on uniswap
+  /// @param _rewardToken address of the token to sell
+  /// @param _path to buy
+  /// @param _amount of `_rewardToken` to sell
+  /// @param _minAmount min amount of `_token` to buy
+  /// @return _amount of _rewardToken sold
+  /// @return _amount received for the sell
+  function _sellReward(address _rewardToken, bytes memory _path, uint256 _amount, uint256 _minAmount)
+    internal override
+    returns (uint256, uint256) {
+    // If 0 is passed as sell amount, we get the whole contract balance
+    if (_amount == 0) {
+      _amount = _contractTokenBalance(_rewardToken);
+    }
+    if (_amount == 0) {
+      return (0, 0);
+    }
+  
+    // Quickswap v3 swap
+    ISwapRouter _swapRouter = ISwapRouter(0xF6Ad3CcF71Abb3E12beCf6b3D2a74C963859ADCd);
+    IERC20Detailed(_rewardToken).safeIncreaseAllowance(address(_swapRouter), _amount);
+    // multi hop swap params
+    ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
+      path: _path,
+      recipient: address(this),
+      deadline: block.timestamp + 100,
+      amountIn: _amount,
+      amountOutMinimum: _minAmount
+    });
+    // do the swap and return the amount swapped and the amount received
+    return (_amount, _swapRouter.exactInput(params));
+  }
+}
