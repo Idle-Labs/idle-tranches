bfaf674a0dbca64a5ff14c06051218476b5c740f
bugduino <william@idle.finance>
2024-01-08 18:03:14 +0100

    FIX: remove useless natspec comment

--- contracts/interfaces/morpho/IMetamorphoSnippets.sol
+++ contracts/interfaces/morpho/IMetamorphoSnippets.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-/// @notice This contract allows Metamorpho users to claim their rewards
 interface IMetamorphoSnippets {
   function supplyAPYVault(address vault) external view returns (uint256 avgSupplyRate);
 }
