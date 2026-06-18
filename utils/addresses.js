96f63209b19cd3cde1c7a76f109456a9301c419f
bugduino <william@idle.finance>
2026-06-18 12:52:39 +0200

    NEW: deploy new implied oracle contract

--- utils/addresses.js
+++ utils/addresses.js
@@ -175,7 +175,7 @@ const mainnetContracts = {
   cdoImplWriteOff: '0x6De6ea8659C8cEa1f2aaf29758E40Ff4C8a1A53F',
   strategyImplWriteOff: '0xc499925d7991FF8204967Ac58455293f2Db3855A',
   oldImpliedPriceOracle: '0xB05CfF1491e9670b4a04ea6BfFb3B2ede4f4C477',
-  impliedPriceOracle: '0x55bdb81026f9a6e9308f7ee727a9124a92ae383e'
+  impliedPriceOracle: '0x1745C027ab3082c3b1019DB69aF295f095500663'
 }
 
 // Polygon
